aca08447f7d55a0ea843aa1001dac027e7715cd7
suman saurabh <suman@gosporto.com>
2017-11-03 13:37:25 +0530

    route changes and including pg gem for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #login routes
-  match 'ams/login'                       => 'authentication#login',              :via => [:get],     :as => :login
+  match 'login'                       => 'authentication#login',              :via => [:get],     :as => :login
 end
