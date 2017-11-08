c6bf53449fa208f3c13d8685665c76892ea78116
suman saurabh <suman@gosporto.com>
2017-11-08 15:48:45 +0530

    adding comparison graphs

--- app/assets/javascripts/ams/web_custom.js
+++ app/assets/javascripts/ams/web_custom.js
@@ -274,6 +274,53 @@ $(function () {
   });
 }
 
+/************* comparison analysis dropdown change script ***********/
+{
+  $(document).on('change', 'select.analysis-dropdown.comparison_analysis', function () {
+    var self = $(this);
+    var attribute_dropdown = self.closest('div.comparision-analysis').find('select.analysis-dropdown.attribute_select');
+    if(self.val() == 'physical'){
+      attribute_dropdown.html('');
+      attribute_dropdown.html('<option value="agility" selected>Agility</option>\n' + '<option value="balance">Balance</option>\n' + '<option value="endurance">Endurance</option>\n' + '<option value="flexibility">Flexibility</option>\n' + '<option value="speed">Speed</option>');
+    }
+    if(self.val() == 'mental'){
+      attribute_dropdown.html('');
+      attribute_dropdown.html('<option value="attention_control" selected>Attention Control</option>\n' + '<option value="negative_energy_control">Negative Energy Control</option>\n' + '<option value="positive_energy_control">Positive Energy Control</option>\n' + '<option value="motivation_level">Motivation Level</option>\n' + '<option value="attitude_control">Attitude Control</option>');
+    }
+    if(self.val() == 'nutrition'){
+      attribute_dropdown.html('');
+      attribute_dropdown.html('<option value="calories" selected>Calories</option>\n' + '<option value="carbohydrates">Carbohydrates</option>\n' + '<option value="protien">Protein</option>\n' + '<option value="fibre">Fibre</option>\n' + '<option value="vitamins_and_minerals">Vitamins and Minerals</option>');
+    }
+  });
+}
+
+/************* comparison analysis attribute dropdown change script ***********/
+{
+  $(document).on('change', 'select.analysis-dropdown.attribute_select', function () {
+    var self = $(this).closest('div.comparision').find('select.analysis-dropdown.comparison_analysis');
+    var attribute_dropdown = self.closest('div.comparision-analysis').find('select.analysis-dropdown.attribute_select');
+    if(attribute_dropdown.val() == 'agility' && self.val() == 'physical'){
+      self.closest('div.comparision').find('div.comparision-graph-container').children().addClass('d-none');
+      self.closest('div.comparision').find('div.comparision-graph-container').find('#agility_comparison_graph').removeClass('d-none');
+    }
+    if(attribute_dropdown.val() == 'balance' && self.val() == 'physical'){
+      self.closest('div.comparision').find('div.comparision-graph-container').children().addClass('d-none');
+      self.closest('div.comparision').find('div.comparision-graph-container').find('#balance_comparison_graph').removeClass('d-none');
+    }
+    if(attribute_dropdown.val() == 'endurance' && self.val() == 'physical'){
+      self.closest('div.comparision').find('div.comparision-graph-container').children().addClass('d-none');
+      self.closest('div.comparision').find('div.comparision-graph-container').find('#endurance_comparison_graph').removeClass('d-none');
+    }
+    if(attribute_dropdown.val() == 'flexibility' && self.val() == 'physical'){
+      self.closest('div.comparision').find('div.comparision-graph-container').children().addClass('d-none');
+      self.closest('div.comparision').find('div.comparision-graph-container').find('#flexibility_comparison_graph').removeClass('d-none');
+    }
+    if(attribute_dropdown.val() == 'speed' && self.val() == 'physical'){
+      self.closest('div.comparision').find('div.comparision-graph-container').children().addClass('d-none');
+      self.closest('div.comparision').find('div.comparision-graph-container').find('#speed_comparison_graph').removeClass('d-none');
+    }
+  });
+}
 
 
 /*------------- Switch Days Schedule script ---------------*/
